d540b7c14b9411acdedfe41a77fd4911ef2eb660
winkidney <winkidney@gmail.com>
2016-08-29 02:54:32 -0700

    Update: Add functional test for reverse decorator order

--- src/cmdtree/tests/functional/test_command.py
+++ src/cmdtree/tests/functional/test_command.py
@@ -1,3 +1,4 @@
+import pytest
 from cmdtree import INT
 from cmdtree import command, argument, option
 
@@ -10,9 +11,24 @@ def run_server(host, reload, port):
     return host, port, reload
 
 
+@command(help="run a http server on given address")
+@argument("host", help="server listen address")
+@option("port", help="server port", type=INT, default=8888)
+def order(port, host):
+    return host, port
+
+
 def test_should_return_given_argument():
     from cmdtree import entry
     result = entry(
         ["run_server", "host", "--reload", "--port", "8888"]
     )
     assert result == ("host", 8888, True)
+
+
+def test_should_reverse_decorator_order_has_no_side_effect():
+    from cmdtree import entry
+    result = entry(
+        ["order", "host", "--port", "8888"]
+    )
+    assert result == ("host", 8888)
