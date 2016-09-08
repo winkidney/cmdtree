db295ff62ac945e03b97e405077e0fa501e4b1c4
winkidney <winkidney@gmail.com>
2016-09-07 23:17:48 -0700

    Update: Update Version to 0.0.3 caused by pypi's limit

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ install_requires = (
 
 setup(
     name='cmdtree',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(HERE, include=['cmdtree']),
     install_requires=install_requires,
     url='https://github.com/winkidney/cmdtree',
