43d7a05500ddae4cd932e0b4e84c6e5430895ab7
winkidney <winkidney@gmail.com>
2017-12-21 08:29:54 +0000

    Refacto: Improve error-message for sub-commands

--- src/cmdtree/templates.py
+++ src/cmdtree/templates.py
@@ -2,8 +2,7 @@ E_MISSING_ARGUMENT = """
 Missing positional arguments: {args}
 """
 
-E_NO_CMD_GIVEN_TPL = """
-No command given, please select from commands below:
+E_NO_CMD_GIVEN_TPL = """Invalid command choice, please select from commands below:
 {
 %s
 }
